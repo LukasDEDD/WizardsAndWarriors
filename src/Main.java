71ab2b73dc4931525cedb4618cd49f1867b00dab
LukasDD82 <lukas.simek.de@gmail.com>
2026-02-04 21:39:32 +0100

    Final version

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Fighter warrior = new Warrior();
+        Wizard wizard = new Wizard();
+
+        System.out.println(warrior); // Fighter is a Warrior
+        System.out.println(wizard);  // Fighter is a Wizard
+
+        // Wizard is initially vulnerable
+        System.out.println("Wizard vulnerable: " + wizard.isVulnerable());
+        System.out.println("Warrior attacks wizard: " + warrior.getDamagePoints(wizard));
+
+        // Wizard prepares a spell
+        wizard.prepareSpell();
+        System.out.println("\nWizard prepares a spell...");
+        System.out.println("Wizard vulnerable: " + wizard.isVulnerable());
+        System.out.println("Wizard attacks warrior: " + wizard.getDamagePoints(warrior));
+
+        // Warrior attacks wizard again
+        System.out.println("Warrior attacks wizard: " + warrior.getDamagePoints(wizard));
+    }
+}
 
